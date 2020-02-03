c633cba5103709d82b63605089aa8231cf5fe5ef
GeorgyMakarov <georgy.v.makarov@gmail.com>
2020-02-03 10:12:56 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming assignment 2 ------------------------------------------------
 
-## Write a short comment describing this function
+## Add matrix into cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## Check if the matrix is square
+  if (dim(x)[1] != dim(x)[2]) {
+    message('Input square matrix')
+  }
+  
+  ## Initialize a variable that will hold the value of invert matrix
+  invM <- NULL
+  
+  ## Define a function that will assign new matrix in parent environment
+  setM <- function(y) {
+    x <<- y
+    invM <<- NULL
+  }
+  
+  ## Get the value of the matrix
+  getM <- function() x
+  
+  ## Set the value of inverse matrix
+  setInvM <- function(solve) invM <<- solve
+  
+  ## Get the value of inverse matrix
+  getInvM <- function() invM
+  list(
+    setM = setM, getM = getM,
+    setInvM = setInvM, getInvM = getInvM
+  )
 }
 
 
-## Write a short comment describing this function
+## Solve inverse of square matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  invM <- x$getInvM()
+  
+  ## For solved matrix return the value from cache
+  if(!is.null(invM)){
+    message('Using cached data')
+    return(invM)
+  }
+  
+  ## If the matrix is new solve inverse new matrix
+  data <- x$getM()
+  invM <- solve(data, ...)
+  x$setInvM(invM)
+  invM
 }
